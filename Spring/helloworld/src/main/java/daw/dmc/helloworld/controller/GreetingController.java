5828999b2a89394a7cab84e1b4327c02a6f72a57
Daniel Montero <danielmonterocaballero@gmail.com>
2025-10-31 17:07:18 +0100

    Saludo a varios usuarios

--- Spring/helloworld/src/main/java/daw/dmc/helloworld/controller/GreetingController.java
+++ Spring/helloworld/src/main/java/daw/dmc/helloworld/controller/GreetingController.java
@@ -4,6 +4,7 @@ import daw.dmc.helloworld.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
@@ -13,8 +14,17 @@ public class GreetingController {
     public String userGreeting(Model model) { // Nombre del controlador
 
         model.addAttribute("message", "¡Bienvenido!"); // Envíamos un String message a la plantilla
-        User userObject = new User("Ximo", "Ximo@example.com"); // Accedemos al modelo para crear un objeto
+        User userObject = new User("Dani", "Dani@example.com"); // Accedemos al modelo para crear un objeto
         model.addAttribute("userObject", userObject); // Envíamos un objeto a la plantilla
         return "user-greeting"; // Plantilla que utilizará este controlador
+    }
+
+    @GetMapping("/helloworld")
+    public String greeting(
+            @RequestParam(required = false) String username1,
+            @RequestParam(required = false)String username2,
+            Model model) {
+        model.addAttribute("message", "Hola " + username1 + " y " + username2);
+        return "greeting";
     }
 }
